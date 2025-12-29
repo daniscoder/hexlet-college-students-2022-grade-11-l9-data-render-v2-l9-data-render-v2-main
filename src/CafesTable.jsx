c1ab015407c3665743013dcfc7575f81c07bb39a
Danis Arslanov <arslanovdk@gmail.com>
2025-12-29 05:44:13 +0500

    add step 1, 2, 3

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,1 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import FilterCafes from "./FilterCafes.jsx";
 
+const CafesTable = () => {
+  const [cafes, setCafes] = useState([]);
+
+  useEffect(() => {
+    const fetchCafes = async () => {
+      const response = await axios.get("/cafes");
+      setCafes(response.data.cafes);
+    };
+    fetchCafes();
+  }, []);
+
+  return (
+    <div className="cafesTable">
+      <FilterCafes />
+      <ul className="cardsList">
+        {cafes.map((cafe) => (
+          <li key={cafe.id} className="card">
+            <img src={cafe.img || "https://via.placeholder.com/150"} alt="" />
+            <h2>{cafe.name}</h2>
+            <p>{cafe.desc}</p>
+            <p>{cafe.address}</p>
+            <p>Subway: {cafe.subwayCode}</p>
+            <p>{cafe.workTime}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CafesTable;
--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,24 @@
+const subways = [
+  { name: "Арбатская", code: "Arbat" },
+  { name: "Александровский сад", code: "Alexanders Garden" },
+  { name: "Московская", code: "Moscow" },
+  { name: "Парк Культуры", code: "Culture" },
+  { name: "Театральная", code: "Theater" },
+];
 
+const FilterCafes = () => {
+  return (
+    <div className="controls">
+      <select name="subway" id="subway">
+        <option value="All">Все</option>
+        {subways.map((subway) => (
+          <option key={subway.code} value={subway.code}>
+            {subway.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default FilterCafes;
